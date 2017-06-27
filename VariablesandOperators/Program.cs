52b1d0ac435b65db2cbd3176b746e4c6f50a5e41
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-06-27 15:10:17 -0400

    added in class excersices

--- VariablesandOperators/Program.cs
+++ VariablesandOperators/Program.cs
@@ -10,6 +10,50 @@ namespace VariablesandOperators
     {
         static void Main(string[] args)
         {
+            string firstName = "Mike";
+            Console.WriteLine(firstName);
+            //Printing the value of birthMonth to the console
+            string birthMonth = "March";
+            Console.WriteLine(birthMonth);
+
+
+
+
+
+            
+
+
+            int age = 65;
+            string why = "because I said so";
+            char firstInitial = 'V';
+
+            float floatnumber = 2.16440330489729484938361032f;
+            Console.WriteLine(floatnumber);
+
+            double doublenumber = 2.16440330489729484938361032d;
+            Console.WriteLine(doublenumber);
+
+            decimal decimalNumber= 2.16440330489729484938361032m;
+            Console.WriteLine(decimalNumber);
+
+            int jessicaAge = 27;
+            int samAge = 42;
+            Console.WriteLine(jessicaAge < samAge);
+
+
+
+            Console.WriteLine("what is the name on reservation");
+            string reserveName = Console.ReadLine();
+           
+            Console.WriteLine("how many people in the party");
+            int numberpeople = int.Parse(Console.ReadLine());
+
+            
+
+
+            
+           
+            
         }
     }
 }
